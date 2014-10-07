fe8d13a646a8a8824572e55ce75679157b9bfec8
silverweed <silverweed1991@gmail.com>
2014-10-07 15:15:22 +0200

    added Volume Bar (todo: add it to BattlePanel)

--- gui/GUIGlobals.java
+++ gui/GUIGlobals.java
@@ -6,5 +6,6 @@ public abstract class GUIGlobals {
 
 	public static final String FONT_FAMILY = "Sans-serif";
 	public static final int FONT_SIZE = 14;
-
+	
+	public static boolean soundOn = true;
 }

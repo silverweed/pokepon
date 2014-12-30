2ca0bd0d0d60efd74f9c8950d890a420375c2601
silverweed <silverweed1991@gmail.com>
2014-12-30 17:27:22 +0000

    added script for creating just server jar

---
+++ data/create_server_only.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ ! -d pokepon ]]; then
+	echo Please, execute this script from outside the pokepon directory.
+	exit 1
+fi
+[[ -f pokepon.jar ]] && rm -f pokepon.jar
+[[ -f manifest.mf ]] && rm -f manifest.mf
+echo 'Name: pokepon-server
+Created-By: silverweed91
+Main-Class: pokepon.net.jack.server.PokeponServer
+' > MANIFEST.MF
+jar cvfm pokepon-server.jar MANIFEST.MF $(< pokepon/data/server_files.txt)
+chmod +x pokepon-server.jar

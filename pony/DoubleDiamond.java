346ea5ed4f3c1e2171e7967d4232f42510918532
silverweed <silverweed1991@gmail.com>
2015-04-11 11:57:05 +0200

    added PartyFavor, DoubleDiamond and sprites

---
+++ pony/DoubleDiamond.java
@@ -0,0 +1,42 @@
+//: pony/DoubleDiamond.java
+
+package pokepon.pony;
+
+import pokepon.enums.*;
+
+/**	Double Diamond
+ *	High Speed,
+ *	good bulk.
+ *
+ * @author silverweed
+ */
+public class DoubleDiamond extends Pony {
+	
+	public DoubleDiamond(int _level) {
+		super(_level);
+		
+		name = "Double Diamond";
+		type[0] = Type.SPIRIT;
+		type[1] = Type.KINDNESS; 
+
+		race = Race.EARTHPONY;
+		sex = Sex.MALE;
+
+		baseHp = 72;
+		baseAtk = 88;
+		baseDef = 84;
+		baseSpatk = 41;
+		baseSpdef = 84;
+		baseSpeed = 111;
+
+		/* Learnable Moves */
+		learnableMoves.put("Tackle",1);
+		learnableMoves.put("Stalking",1);
+		learnableMoves.put("Hidden Talent",1);
+		learnableMoves.put("Mirror Pond",1);
+		learnableMoves.put("Practice",1);
+		learnableMoves.put("Hopeful Strike",1);
+		learnableMoves.put("Dodge",1);
+		learnableMoes.put/("Crazy Stunt",1);
+	}
+}
---
+++ pony/PartyFavor.java
@@ -0,0 +1,38 @@
+//: pony/PartyFavor.java
+
+package pokepon.pony;
+
+import pokepon.enums.*;
+
+/**	Party Favor
+ *	High Speed,
+ *	good bulk.
+ *
+ * @author silverweed
+ */
+public class PartyFavor extends Pony {
+	
+	public PartyFavor(int _level) {
+		super(_level);
+		
+		name = "Party Favor";
+		type[0] = Type.LAUGHTER;
+		type[1] = Type.HONESTY; 
+
+		race = Race.UNICORN;
+		sex = Sex.MALE;
+
+		baseHp = 60;
+		baseAtk = 80;
+		baseDef = 100;
+		baseSpatk = 100;
+		baseSpdef = 80;
+		baseSpeed = 60;
+
+		/* Learnable Moves */
+		learnableMoves.put("Tackle",1);
+		learnableMoves.put("Hidden Talent",1);
+		learnableMoves.put("Mirror Pond",1);
+		learnableMoves.put("Dodge",1);
+	}
+}

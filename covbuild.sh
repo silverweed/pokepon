3d4d8c1543b1cdfe0facfde5d7cdd27d90b99c0b
silverweed <silverweed1991@gmail.com>
2015-08-04 16:41:38 +0200

    updated covbuild

--- covbuild.sh
+++ covbuild.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Settings
+
+PROJECT_NAME=pokepon
+EMAIL=silverweed1991@gmail.com
+COVERITY_PATH=$HOME/Public/cov-analysis-linux64-7.6.0/bin
+
+###########
 
 getbuild() {
 	git describe --always --long --dirty
@@ -8,10 +16,11 @@ getreadiness() {
 	tail cov-int/build-log.txt | grep compilation | cut -f2 -d\( | cut -f1 -d%
 }
 
-PATH=$PATH:$HOME/Public/cov-analysis-linux64-7.6.0/bin 
-rm -f pokepon.tgz 
-if (cov-build --dir cov-int make); then
-	tar cvfz pokepon.tgz cov-int
+PATH=$PATH:$COVERITY_PATH
+make clean
+rm -f ${PROJECT_NAME}.tgz
+if (cov-build --dir cov-int make -j); then
+	tar cvfz ${PROJECT_NAME}.tgz cov-int
 fi
 
 if [[ $? == 0 ]]; then
@@ -22,18 +31,18 @@ if [[ $? == 0 ]]; then
 	else
 		echo "[ OK ] ${R}% of the compilation units are ready."
 	fi
-	echo "Submit new build?"
+	echo "Submit new build? ($(du -sh ${PROJECT_NAME}.tgz | cut -f1))"
 	select ANS in "Submit" "Abort"; do
 		case $ANS in
 			Submit)
 				echo Submitting... >&2
 				set -x
 				curl	--form token=$(< ./cov-token) \
-					--form email=silverweed1991@gmail.com \
-					--form file=@./pokepon.tgz \
-					--form version="0.1" \
-					--form description="Pokepon build rev.$(getbuild)" \
-					https://scan.coverity.com/builds?project=pokepon
+					--form email=$EMAIL \
+					--form file=@./${PROJECT_NAME}.tgz \
+					--form version="0.0" \
+					--form description="${PROJECT_NAME} build rev.$(getbuild)" \
+					https://scan.coverity.com/builds?project=${PROJECT_NAME}
 				set +x
 				echo Done. >&2
 				exit 0
